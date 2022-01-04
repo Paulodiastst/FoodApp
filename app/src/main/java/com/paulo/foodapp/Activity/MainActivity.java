c8ddee6ea2f6f90e6e1efc6faf25c1eb568f156d
Paulodiastst <paulodiastst@hotmail.com>
2022-01-04 18:10:32 -0300

    Start Intro activity

---
+++ app/src/main/java/com/paulo/foodapp/Activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.paulo.foodapp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.paulo.foodapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
